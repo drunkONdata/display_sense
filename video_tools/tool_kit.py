866bed86975a9897eac59872fc7fd34612ac8ca4
3point14thon <chris.alan.bowers@gmail.com>
2018-12-01 09:37:54 -0800

    functions now contain doc strings

--- video_tools/tool_kit.py
+++ video_tools/tool_kit.py
@@ -4,21 +4,58 @@ from time import sleep
 from imutils.video import VideoStream
 
 def write_to_video(name, images):
+    """
+    This function writes a list of time_stamp,images into a video named using
+    the name parameter and the first time stamp in the image list.
+
+    Args:
+        name: Name of the video as a string.
+        images: A list of tuples containing time stamps and
+                images, (time stamp, image).
+
+    Returns:
+        None
+    """
+    time_stamped_name = f'{images[0][0]}{name}'
     height, width, layers = images[0][1].shape
     video_fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    video = cv2.VideoWriter(name, video_fourcc, 30, (width,height))
+    video = cv2.VideoWriter(time_stamped_name, video_fourcc, 30, (width,height))
     for time_stamp, image in images:
         video.write(image)
     video.release()
     cv2.destroyAllWindows()
 
 def write_to_picseries(series_name, images):
+    """
+    This function writes a list of time_stamp,images into a collection of
+    saved images having the of time stamp + series name parameter.
+
+    Args:
+        name: Name of the picture series as a string.
+        images: A list of tuples containing time stamps and
+                images, (time stamp, image).
+
+    Returns:
+        None
+    """
     height, width, layers = images[0][1].shape
     for time_stamp, image in images:
-        cv2.imwrite(f'{series_name}{time_stamp}.png',image)
+        cv2.imwrite(f'{time_stamp}{series_name}',image)
     cv2.destroyAllWindows()
 
 def record_webcam(duration, frame_rate):
+    """
+    Uses the local webcam to record for the duration and frame rate specified.
+    Returns a list of tuples containing a time stamp and an image.
+
+    Args:
+        duration: The amount of time to record for in seconds as an int.
+        frame_rate: The number of images to record each second as an int.
+
+    Returns:
+        A list of tuples containing a time stamp an image,
+        (time stamp,(image)).
+    """
     vs = VideoStream()
     vs.start()
     n = 0
