0711d3d67de3f1a9c80dbe48c52d96ab07826a4f
3point14thon <chris.alan.bowers@gmail.com>
2018-12-01 08:37:29 -0800

    webcam recorder function now includes datetime with each frame

--- video_tools/tool_kit.py
+++ video_tools/tool_kit.py
@@ -1,13 +1,33 @@
+import cv2
+import datetime
+from time import sleep
+from imutils.video import VideoStream
+
 def write_to_video(name, images):
-    height, width, layers = images[0].shape
+    height, width, layers = images[0][1].shape
     video_fourcc = cv2.VideoWriter_fourcc(*'XVID')
     video = cv2.VideoWriter(name, video_fourcc, 30, (width,height))
-    for image in images:
+    for time_stamp, image in images:
         video.write(image)
     video.release()
+    cv2.destroyAllWindows()
 
-def write_to_picseries(images, series_name):
-    height, width, layers = images[0].shape
+def write_to_picseries(series_name, images):
+    height, width, layers = images[0][1].shape
+    for time_stamp, image in images:
+        cv2.imwrite(f'{series_name}{time_stamp}.png',image)
+    cv2.destroyAllWindows()
 
-    for frame, image in enumerate(images):
-        cv2.imwrite(f'{series_name}{frame}.png',image)
+def record_webcam(duration, frame_rate):
+    vs = VideoStream()
+    vs.start()
+    n = 0
+    images = []
+    while n < duration * frame_rate:
+        image = vs.read()
+        if vs.read() is not None:
+            images.append((datetime.datetime.now(), image))
+            n += 1
+        sleep(1/frame_rate)
+    vs.stop()
+    return images
